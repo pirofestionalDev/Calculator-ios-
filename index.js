e9622273e87f0bef5a4b8b4b15e884f8d688e710
pirofestionalDev <r4roggit1@gmail.com>
2024-10-20 01:18:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const display = document.getElementById(`display`);//the operation class
+//inputValue, clearDisplay, calculate, backSpace
+function inputValue(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function backSpace(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Wrong Format";
+    }
+}
